53cdef3a6ed5e5d65d0bbe73319feb3af6df1391
simon040102 <simon040102@gmail.com>
2022-06-05 17:50:38 +0800

    no message

--- js/all.js
+++ js/all.js
@@ -4,4 +4,12 @@ $(document).ready(function () {
     $('.mobile-menu-items').toggleClass('active');
     $('active').slideToggle(1000);
   });
+
+  $('.receipt-type a').click(function (e) {
+    e.preventDefault();
+    $(this).addClass('receipt-active').siblings().removeClass('receipt-active');
+  });
+
 });
+
+
