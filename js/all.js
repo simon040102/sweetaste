97fc6644132f4b1b52bc0045365f6035140edac6
simon040102 <simon040102@gmail.com>
2022-06-08 15:27:21 +0800

    modify_receiptFunction

--- js/all.js
+++ js/all.js
@@ -7,9 +7,24 @@ $(document).ready(function () {
 
   $('.receipt-type a').click(function (e) {
     e.preventDefault();
+    if ($(this).attr('class') == 'mail-receipt') {
+        $('.receipt-mail').removeClass('d-none');
+        $('.receipt-address').removeClass('d-block');
+    }
+    if ($(this).attr('class') == 'send-receipt') {
+        $('.receipt-address').addClass('d-block');
+        $('.receipt-mail').addClass('d-none');
+    }
     $(this).addClass('receipt-active').siblings().removeClass('receipt-active');
   });
 
+  // $('.mail-receipt').click(function(e){
+  //   e.preventDefault();
+  //   $('.receipt-mail').toggleClass('d-block');
+  // });
+
+  // $('.send-receipt').click(function (e) {
+  //   e.preventDefault();
+  //   $('.receipt-address').toggleClass('d-block');
+  // });
 });
-
-
