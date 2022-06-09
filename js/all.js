9b4534a2ae158a1bffebfb18bab42299950491b1
simon040102 <simon040102@gmail.com>
2022-06-09 14:35:05 +0800

    點擊愛心分反紅

--- js/all.js
+++ js/all.js
@@ -1,30 +1,28 @@
 $(document).ready(function () {
+  //menu
   $('.mobile-menu').on('click', function (e) {
     e.preventDefault();
     $('.mobile-menu-items').toggleClass('active');
     $('active').slideToggle(1000);
   });
 
+  //愛心點擊
+  $('.heart a i').click(function(e){
+    e.preventDefault();
+    $(this).toggleClass('fas')
+  })
+
+  //發票方式
   $('.receipt-type a').click(function (e) {
     e.preventDefault();
     if ($(this).attr('class') == 'mail-receipt') {
-        $('.receipt-mail').removeClass('d-none');
-        $('.receipt-address').removeClass('d-block');
+      $('.receipt-mail').removeClass('d-none');
+      $('.receipt-address').removeClass('d-block');
     }
     if ($(this).attr('class') == 'send-receipt') {
-        $('.receipt-address').addClass('d-block');
-        $('.receipt-mail').addClass('d-none');
+      $('.receipt-address').addClass('d-block');
+      $('.receipt-mail').addClass('d-none');
     }
     $(this).addClass('receipt-active').siblings().removeClass('receipt-active');
   });
-
-  // $('.mail-receipt').click(function(e){
-  //   e.preventDefault();
-  //   $('.receipt-mail').toggleClass('d-block');
-  // });
-
-  // $('.send-receipt').click(function (e) {
-  //   e.preventDefault();
-  //   $('.receipt-address').toggleClass('d-block');
-  // });
 });
